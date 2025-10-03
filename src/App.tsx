0d5df05c95822eea9b399a8503f380e761fb8bb3
Raymond <raymond.f.kneipp@gmail.com>
2025-10-03 13:12:38 -0400

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import { Button } from "./components/ui/button";
 export function App() {
 	return (
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-			<h1>BioRead</h1>
+			<h1 className="font-bold text-2xl">BioRead</h1>
+
+			<p className="font-sans">The quick brown fox jumps over the lazy dog</p>
+			<p className="font-dyslexic">
+				The quick brown fox jumps over the lazy dog
+			</p>
 			<ReadingList />
 			<Button>Click Me</Button>
 			<ModeToggle />
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
+import "@fontsource-variable/montserrat";
+import "@fontsource/opendyslexic/400.css";
+import "@fontsource/opendyslexic/700.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
