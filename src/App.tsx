7881dc21ed6ca3d68445c5626f9cdf3dd33f0f04
Raymond <raymond.f.kneipp@gmail.com>
2025-10-03 12:12:31 -0400

    add light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
 import { Button } from "./components/ui/button";
 
 export function App() {
 	return (
-		<>
-			<h1>Vite + React</h1>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<h1>BioRead</h1>
 			<Button>Click Me</Button>
-		</>
+			<ModeToggle />
+		</ThemeProvider>
 	);
 }
