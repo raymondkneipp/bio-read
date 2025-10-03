31392beb84cfea6222ef9b0bc96e156d615a4049
Raymond <raymond.f.kneipp@gmail.com>
2025-10-03 12:09:51 -0400

    add shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
 	return (
 		<>
 			<h1>Vite + React</h1>
+			<Button>Click Me</Button>
 		</>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -12,4 +13,9 @@ export default defineConfig({
 			},
 		}),
 	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
