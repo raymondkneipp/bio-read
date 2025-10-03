0a88ab5b9483a6ad3a69da6e4ce3b2d6b64c0a7d
Raymond <raymond.f.kneipp@gmail.com>
2025-10-03 12:55:56 -0400

    reading cards

--- src/components/reading-list.tsx
+++ src/components/reading-list.tsx
@@ -1,14 +1,14 @@
 import {
 	Card,
-	CardAction,
 	CardContent,
 	CardDescription,
 	CardFooter,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import { PencilIcon, PlusIcon, TrashIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { readings } from "@/mock/readings";
-import { PlusIcon } from "lucide-react";
 
 export function ReadingList() {
 	return (
@@ -26,15 +26,21 @@ export function ReadingList() {
 				<Card>
 					<CardHeader>
 						<CardTitle>{reading.title}</CardTitle>
-						<CardDescription>Card Description</CardDescription>
-						<CardAction>Card Action</CardAction>
+						<CardDescription>
+							{reading.createdAt.toLocaleString()}
+						</CardDescription>
 					</CardHeader>
 					<CardContent>
 						{/* TODO: Show preview of where user left off */}
 						<p className="line-clamp-2">{reading.content.slice(0, 400)}</p>
 					</CardContent>
-					<CardFooter>
-						<p>Card Footer</p>
+					<CardFooter className="justify-end">
+						<Button variant="ghost" size="icon">
+							<PencilIcon />
+						</Button>
+						<Button variant="ghost" size="icon">
+							<TrashIcon />
+						</Button>
 					</CardFooter>
 				</Card>
 			))}
